b7daade17b1563973e923b7802bcd723fa6f3550
Jeisson <jeda1989@gmail.com>
2024-12-01 04:07:29 -0500

    create Home component

--- src/redux/authSlice.tsx
+++ src/redux/authSlice.tsx
@@ -1,23 +1,26 @@
-// src/redux/authSlice.ts
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface AuthState {
   isAuthenticated: boolean;
+  user: string | null;
 }
 
 const initialState: AuthState = {
   isAuthenticated: false,
+  user: null,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    login: (state) => {
+    login: (state, action: PayloadAction<string>) => {
       state.isAuthenticated = true;
+      state.user = action.payload;
     },
     logout: (state) => {
       state.isAuthenticated = false;
+      state.user = null; 
     },
   },
 });
